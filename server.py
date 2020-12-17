88c397f2341160af952bbd02ff928bc16edf964d
John Dunne <G00273895@gmit.ie>
2020-12-17 18:22:43 +0000

    Added session counter to server.py

--- server.py
+++ server.py
@@ -1,15 +1,45 @@
-from flask import Flask, jsonify, request, abort
+# Data Representation big project
+# John Dunne G00273895
+# application server to be run in virtual environment 
+
+# import flask and dependencies 
+from flask import Flask, jsonify, request, abort, session
 # import a new instance of the bullDAO class from the BullDAO file
 from bullDAO import bullDAO
 
 app = Flask(__name__, static_url_path='', static_folder='staticpages')
-
+app.secret_key = 'MyS3cr£tk€y'
 #app = Flask(__name__)
 
 #curl "http://127.0.0.1:5000"
-@app.route('/')
+@app.route("/")
 def index():
-    return "Hello, World!"
+    count=0
+    count+=1
+
+    if not 'counter' in session:
+        session['counter'] =0
+        print("new session")
+
+    sessionCount=session['counter']
+    sessionCount+=1
+    session['counter']=sessionCount
+
+    
+
+    pageContent="<h1>counts</h1>" +\
+        "session Count ="+str(sessionCount) +\
+        "<br/>this Count ="+str(count)
+    
+    return pageContent
+
+# function to clear the session counter
+@app.route('/clear')
+def clear():
+    #session.clear()
+    session.pop('counter',None)   
+
+    return "done" 
 
 # get all
 #curl "http://127.0.0.1:5000/bulls"
